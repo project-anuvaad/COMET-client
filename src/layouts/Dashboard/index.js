d28b166d8435390a3c078b98e5b5b28320ef1077
hassanamin994 <hassanmamin994@gmail.com>
2020-04-11 16:29:28 +0200

    change socket route

--- src/layouts/Dashboard/index.js
+++ src/layouts/Dashboard/index.js
@@ -110,7 +110,7 @@ class Dashboard extends React.Component {
             this.props.fetchOrganization(this.props.organization._id)
         }
         this.websocketConnection = websockets.createWebsocketConnection(WEBSOCKET_SERVER_URL, {
-            path: '/ws',
+            path: '/socket.io',
             transports: ['websocket'],
             secure: true,
         })
